d06b83fed970ba9d9efc74b7c9de49f22ef005f4
travispbuckley <travispbuckley@lewisu.edu>
2018-08-10 19:37:58 -0500

    remove node modules folder and use lodash functions

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ console.log('Starting app.js');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
+const _ = require('lodash');
 
-var result = notes.addNote();
-console.log(result);
+console.log(_.isString(true));
+console.log(_.isString('Travis'));
 
-var result2 = notes.add(1, 2);
-console.log(result2);
+var filteredArray = _.uniq(['Travis', 'Andrew', 'Travis', 1, 2, 1]);
+console.log(filteredArray);
+
+
+// console.log(notes.addNote());
+// console.log(notes.add(1, 2));
 
 // var user = os.userInfo();
-
-
 // fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
